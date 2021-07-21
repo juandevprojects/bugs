c2f3d9dac4a1ce0470d22cfd70788a9eadb56dcb
juandevprojects <48599196+juandevprojects@users.noreply.github.com>
2021-07-21 18:31:13 +0200

    Update simple_imputer_warning.py

--- simple_imputer_warnings/simple_imputer_warning.py
+++ simple_imputer_warnings/simple_imputer_warning.py
@@ -2,8 +2,8 @@
 import pandas as pd
 from sklearn.impute import SimpleImputer
 
-# data_source = 'pickle' # change to 'csv' to download data from .csv file
-data_source = 'csv'
+data_source = 'pickle' # change to 'csv' to download data from .csv file
+# data_source = 'csv'
 if data_source == 'pickle':
     numerical_vars = pd.read_pickle('numerical_vars.pkl')
 elif data_source == 'csv':
@@ -23,7 +23,7 @@ imputed_X_train = pd.DataFrame(my_imputer.fit_transform(df))
 
 # %%
 # change col values
-col = '2' # cols ['2', '3', '4', '5', '6] Impute bad. Raise  RuntimeWarning
+col = '2' # cols ['2', '3', '4', '5', '6] Impute bad. Raise  RuntimeWarning (only using data in pickle format)
 df = numerical_vars[[col]].copy()
 my_imputer = SimpleImputer(strategy='mean')
 imputed_X_train = pd.DataFrame(my_imputer.fit_transform(df))
